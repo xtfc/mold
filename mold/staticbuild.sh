73483b3bc76307854541c0466bb8bcad50410394
John Weachock <jweachock@gmail.com>
2019-08-01 14:15:17 -0400

    Add staticbuild recipe

---
+++ mold/staticbuild.sh
@@ -0,0 +1,12 @@
+local_file=${EXE:-moldy}
+
+echo "Build..."
+docker build -t mold .
+echo "Start..."
+container=$(docker run --rm --detach --tty mold)
+echo "Copy..."
+docker cp $container:/home/rust/mold $local_file
+echo "Kill..."
+docker kill $container
+echo "Done."
+ls $local_file
