a12f38d16029ac9acb5b5ad535942a2fd05c80fe
John Weachock <jweachock@gmail.com>
2019-09-11 15:01:52 -0400

    Force getmoldsh.sh to overwrite

--- getmoldsh.sh
+++ getmoldsh.sh
@@ -10,25 +10,23 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-if [ ! -f $EXE ]; then
-  # decide whether we can use curl or wget
-  if hash curl 2>/dev/null; then
-    CMD="curl -sSfL $URL -o $EXE"
-  elif hash wget 2>/dev/null; then
-    CMD="wget -q $URL -O $EXE"
-  else
-    printf $RED"Error:"$NC" could not find curl or wget\n"
-    exit 1
-  fi
+# decide whether we can use curl or wget
+if hash curl 2>/dev/null; then
+  CMD="curl -sSfL $URL -o $EXE"
+elif hash wget 2>/dev/null; then
+  CMD="wget -q $URL -O $EXE"
+else
+  printf $RED"Error:"$NC" could not find curl or wget\n"
+  exit 1
+fi
 
-  # download or exit
-  printf $GREEN" Downloading"$NC" mold.sh $BLUE$REF$NC\r"
-  if $CMD; then
-    chmod +x $EXE
-    printf $GREEN"  Downloaded"$NC" mold.sh $BLUE$REF$NC\n"
-  else
-    printf $RED"Error:"$NC" could not download mold.sh $BLUE$REF$NC\n"
-    rm $EXE
-    exit 1
-  fi
+# download or exit
+printf $GREEN" Downloading"$NC" mold.sh $BLUE$REF$NC\r"
+if $CMD; then
+  chmod +x $EXE
+  printf $GREEN"  Downloaded"$NC" mold.sh $BLUE$REF$NC\n"
+else
+  printf $RED"Error:"$NC" could not download mold.sh $BLUE$REF$NC\n"
+  rm $EXE
+  exit 1
 fi
