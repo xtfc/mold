db2b9dee22fa92465067bb892566f3ff240f8309
John Weachock <jweachock@gmail.com>
2019-09-11 15:00:57 -0400

    Add getmoldsh.sh to get mold.sh
    That's confusing

---
+++ getmoldsh.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# This script will download the mold.sh executable from $URL as $EXE, then exit
+
+REF="master"
+EXE="mold.sh"
+URL="https://raw.githubusercontent.com/xtfc/mold/$REF/mold.sh"
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+if [ ! -f $EXE ]; then
+  # decide whether we can use curl or wget
+  if hash curl 2>/dev/null; then
+    CMD="curl -sSfL $URL -o $EXE"
+  elif hash wget 2>/dev/null; then
+    CMD="wget -q $URL -O $EXE"
+  else
+    printf $RED"Error:"$NC" could not find curl or wget\n"
+    exit 1
+  fi
+
+  # download or exit
+  printf $GREEN" Downloading"$NC" mold.sh $BLUE$REF$NC\r"
+  if $CMD; then
+    chmod +x $EXE
+    printf $GREEN"  Downloaded"$NC" mold.sh $BLUE$REF$NC\n"
+  else
+    printf $RED"Error:"$NC" could not download mold.sh $BLUE$REF$NC\n"
+    rm $EXE
+    exit 1
+  fi
+fi
