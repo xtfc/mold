cfccd542ade80b7b8c20e6cf23c23b4160020b49
John Weachock <jweachock@gmail.com>
2019-09-18 14:07:19 -0400

    Version bump to 0.3.2

--- mold.sh
+++ mold.sh
@@ -3,7 +3,7 @@
 # all arguments through to it. This simplifies usage on CI/CD platforms as well
 # as for users who haven't installed mold yet.
 
-VER="0.3.1"
+VER="0.3.2"
 EXE="./.mold-$VER"
 URL="https://github.com/xtfc/mold/releases/download/v$VER/mold-$VER"
 
